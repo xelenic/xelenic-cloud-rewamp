bada5c13a21a466842ba3ba389b53b694f9cbe3e
sanjayaharshana <sanjayaharasana@gmail.com>
2024-04-13 18:31:02 +0530

    Create Product Items

--- database/migrations/2024_04_13_122628_create_products_table.php
+++ database/migrations/2024_04_13_122628_create_products_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name');
+            $table->string('icon')->nullable();
+            $table->float('price');
+            $table->text('description')->nullable();
+            $table->text('short_description');
+            $table->enum('status',['active','inactive']);
+            $table->string('cover_photo')->nullable();
+            $table->string('category_id');
+            $table->boolean('is_available_api');
+            $table->boolean('showing_frontend');
+            $table->string('product_type')->comment('sass_tool,in-build_application,self_hosted_application');
+            $table->string('payment_type')->comment('payment_required,free,paid_item');
+            $table->json('payment_rules')->nullable();
+            $table->json('faq')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_04_13_124413_create_purchased_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchased_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('purchased_item_id');
+            $table->string('item_type')->comment('service or product');
+            $table->float('price');
+            $table->string('payment_method')->comment('payment_method type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchased_items');
+    }
+};
