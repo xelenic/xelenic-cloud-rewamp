8e821578cf2cbcf84800d940cf0b75d609e48c4f
sanjayaharshana <sanjayaharasana@gmail.com>
2024-04-22 02:31:46 +0530

    Forum Install

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,10 +2,6 @@
 @extends('layouts.xelanic')
 
 @section('content')
-<div class="header">
-    <!-- Content before waves -->
-    <!-- Include the content before waves -->
-</div>
 <!-- Content starts -->
 <div class="content flex">
     <p>Copyright (c) Xelenic Pvt Ltd </p>
