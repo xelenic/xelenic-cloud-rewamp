46b5d2120f46d284f96a42439ccb7f48370b10ee
sanjayaharshana <sanjayaharasana@gmail.com>
2024-05-03 03:58:48 +0530

    Resource Upates

--- app/Http/Controllers/ProductPageController.php
+++ app/Http/Controllers/ProductPageController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductPageController extends Controller
 {
     public function show($slug)
     {
-        return view('frontend.pages.products.product', compact('slug'));
+        $getProducts = Products::where('slug', $slug)->first();
+        return view('frontend.pages.products.product', [
+            'productDetails' => $getProducts
+        ]);
     }
 }
