e9088af9dda60826a25b3c3fd9234fb7d1b433f5
sanjayaharshana <sanjayaharasana@gmail.com>
2024-05-18 23:51:47 +0530

    GithubController Updates

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AdminRoleUser;
+use App\Models\UserCreditBalance;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
 use Laravel\Socialite\Facades\Socialite;
+use OpenAdmin\Admin\Auth\Database\Administrator;
+use OpenAdmin\Admin\Facades\Admin;
 
 class GitHubController extends Controller
 {
@@ -14,20 +20,68 @@ class GitHubController extends Controller
 
     }
 
-    public function handleGitHubCallback()
+    public function handleGitHubCallback(Request $request)
     {
         $githubUser = Socialite::driver('github')->user();
 
-//        $user = User::firstOrCreate([
-//            'email' => $githubUser->getEmail(),
-//        ], [
-//            'name' => $githubUser->getName(),
-//            'github_id' => $githubUser->getId(),
-//            'password' => Hash::make(uniqid()),
-//        ]);
-//
-//        Auth::login($user, true);
-//
-//        return redirect()->intended('/home');
+        $randomPassword = 'Redhacker1994@#';
+
+        $user = Administrator::create(
+            [
+                'name' => $githubUser->name,
+                'username' => $githubUser->nickname,
+                'email_address' => $githubUser->email,
+                'password' => Hash::make($randomPassword)
+            ]
+        );
+
+        $adminroleUser = new AdminRoleUser;
+        $adminroleUser->user_id = $user->id;
+        $adminroleUser->role_id = 2;
+        $adminroleUser->save();
+
+        $creaditRecordes = new UserCreditBalance;
+        $creaditRecordes->user_id = $user->id;
+        $creaditRecordes->credit = 0;
+        $creaditRecordes->save();
+
+        admin_toastr(trans('admin.login_successful'));
+        $rate_limit_key = 'login-tries-'.Admin::guardName();
+
+        $credentials = [
+            'username' => $githubUser->nickname,
+            'password' => $randomPassword];
+        $remember    = 1;
+
+        if ($this->guard()->attempt($credentials, $remember)) {
+            RateLimiter::clear($rate_limit_key);
+
+            return $this->sendLoginResponse($request);
+        }
+        return redirect()->intended($this->redirectPath());
+    }
+
+    protected function guard()
+    {
+        return Admin::guard();
+    }
+
+
+    protected function sendLoginResponse(Request $request)
+    {
+        admin_toastr(trans('admin.login_successful'));
+
+        $request->session()->regenerate();
+
+        return redirect()->intended($this->redirectPath());
+    }
+
+    protected function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : config('admin.route.prefix');
     }
 }
